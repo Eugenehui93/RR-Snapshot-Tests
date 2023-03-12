9ec610f7462089eac27dc9e582e740635973879d
Yee Ching Hui <eugenehui93@Yees-MBP.lan>
2023-03-12 15:38:21 -0700

    add profile pic

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import renderer from 'react-test-renderer'
+
+test('renders text MickeyMouse', () => {
+  render(<App/>)
+  const nameElement = screen.getByText(/mickey mouse/i)
+  expect(nameElement).toBeInTheDocument()
+})
+
